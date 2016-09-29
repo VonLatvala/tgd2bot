d9f1e6e94d40681b8ae1b72fc219cb2402e535b5
Vonlatvala <axel@latvala.com>
2016-09-29 22:46:56 +0300

    Fixed trying to add users with @ in their username

--- bot.php
+++ bot.php
@@ -44,6 +44,8 @@
         $numParticipants = count($chatData);
         if($numParticipants > 4)
             return 'Sorry, full party already';
+        if(substr($username, 0, 1) === '@')
+            $username = substr($username, 1);
         if(empty($username))
             return 'Having no username is shameful. I do not permit you to sign up without one.';
         if(in_array($username, $chatData))
